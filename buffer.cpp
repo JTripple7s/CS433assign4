7b88e83c91d84c77bf4a30f52ae3b0b34a7f7354
Joel Burlingame <jtburlingame777@gmail.com>
2025-04-16 15:37:56 -0700

    completed pc

--- buffer.cpp
+++ buffer.cpp
@@ -1,13 +1,132 @@
 /**
-* Assignment 4: Producer Consumer Problem
+ * Assignment 4: Producer Consumer Problem
  * @file buffer.cpp
  * @author Joel Burlingame
  * @brief Implementation file for the buffer class
  * @version 0.1
  */
 
-// You must complete the all parts marked as "TODO". Delete "TODO" after you are done.
-// Remember to add sufficient and clear comments to your code
-#include "buffer.h"
-
-// TODO: Add your implementation of the buffer class here
+ #include "buffer.h"
+ #include <iostream>
+ 
+ /**
+  * @brief Construct a new Buffer object with the given size
+  * @param size The maximum capacity of the buffer
+  */
+ Buffer::Buffer(int size) {
+     this->size = size;
+     pthread_mutex_init(&mutex, NULL);
+     pthread_cond_init(&full, NULL);
+     pthread_cond_init(&empty, NULL);
+ }
+ 
+ /**
+  * @brief Destroy the Buffer object and clean up resources
+  */
+ Buffer::~Buffer() {
+     pthread_mutex_destroy(&mutex);
+     pthread_cond_destroy(&full);
+     pthread_cond_destroy(&empty);
+ }
+ 
+ /**
+  * @brief Insert an item into the buffer
+  * @param item The item to insert
+  * @return true if successful, false otherwise
+  */
+ bool Buffer::insert_item(buffer_item item) {
+     pthread_mutex_lock(&mutex);
+     
+     // Wait while the buffer is full
+     while (is_full()) {
+         pthread_cond_wait(&full, &mutex);
+     }
+     
+     // Insert the item
+     buf.push(item);
+     
+     // Signal that the buffer is not empty
+     pthread_cond_signal(&empty);
+     pthread_mutex_unlock(&mutex);
+     
+     return true;
+ }
+ 
+ /**
+  * @brief Remove an item from the buffer
+  * @param item Pointer to store the removed item
+  * @return true if successful, false otherwise
+  */
+ bool Buffer::remove_item(buffer_item *item) {
+     pthread_mutex_lock(&mutex);
+     
+     // Wait while the buffer is empty
+     while (is_empty()) {
+         pthread_cond_wait(&empty, &mutex);
+     }
+     
+     // Remove the item
+     *item = buf.front();
+     buf.pop();
+     
+     // Signal that the buffer is not full
+     pthread_cond_signal(&full);
+     pthread_mutex_unlock(&mutex);
+     
+     return true;
+ }
+ 
+ /**
+  * @brief Get the maximum capacity of the buffer
+  * @return The size of the buffer
+  */
+ int Buffer::get_size() {
+     return size;
+ }
+ 
+ /**
+  * @brief Get the current number of items in the buffer
+  * @return The count of items
+  */
+ int Buffer::get_count() {
+     return buf.size();
+ }
+ 
+ /**
+  * @brief Check if the buffer is empty
+  * @return true if empty, false otherwise
+  */
+ bool Buffer::is_empty() {
+     return buf.empty();
+ }
+ 
+ /**
+  * @brief Check if the buffer is full
+  * @return true if full, false otherwise
+  */
+ bool Buffer::is_full() {
+     return buf.size() >= size;
+ }
+ 
+ /**
+  * @brief Print the current contents of the buffer
+  */
+ void Buffer::print_buffer() {
+     std::cout << "Buffer: [";
+     
+     // Create a temporary copy of the queue
+     std::queue<buffer_item> temp = buf;
+     
+     if (!temp.empty()) {
+         std::cout << temp.front();
+         temp.pop();
+         
+         while (!temp.empty()) {
+             std::cout << ", " << temp.front();
+             temp.pop();
+         }
+     }
+     
+     std::cout << "]" << std::endl;
+ }
+ 
--- main.cpp
+++ main.cpp
@@ -1,65 +1,107 @@
 /**
-* Assignment 4: Producer Consumer Problem
+ * Assignment 4: Producer Consumer Problem
  * @file main.cpp
  * @author Joel Burlingame
  * @brief The main program for the producer consumer problem.
  * @version 0.1
  */
-//You must complete the all parts marked as "TODO". Delete "TODO" after you are done.
-// Remember to add sufficient and clear comments to your code
-#include <iostream>
-#include "buffer.h"
-#include <unistd.h>
 
-using namespace std;
-
-// global buffer object
-Buffer buffer;
-
-// Producer thread function
-// TODO: Add your implementation of the producer thread here
-void *producer(void *param) {
-    // Each producer insert its own ID into the buffer
-    // For example, thread 1 will insert 1, thread 2 will insert 2, and so on.
-    buffer_item item = *((int *) param);
-
-    while (true) {
-        /* sleep for a random period of time */
-        usleep(rand()%1000000);
-        // TODO: Add synchronization code here
-        if (buffer.insert_item(item)) {
-            cout << "Producer " << item << ": Inserted item " << item << endl;
-            buffer.print_buffer();
-        } else {
-            cout << "Producer error condition"  << endl;    // shouldn't come here
-        }
-    }
-}
-
-// Consumer thread function
-// TODO: Add your implementation of the consumer thread here
-void *consumer(void *param) {
-    buffer_item item;
-
-    while (true) {
-        /* sleep for a random period of time */
-        usleep(rand() % 1000000);
-        // TODO: Add synchronization code here
-        if (buffer.remove_item(&item)) {
-            cout << "Consumer " << item << ": Removed item " << item << endl;
-            buffer.print_buffer();
-        } else {
-            cout << "Consumer error condition" << endl;    // shouldn't come here
-        }
-    }
-}
-
-int main(int argc, char *argv[]) {
-    /* TODO: 1. Get command line arguments argv[1],argv[2],argv[3] */
-    /* TODO: 2. Initialize buffer and synchronization primitives */
-    /* TODO: 3. Create producer thread(s).
-     * You should pass an unique int ID to each producer thread, starting from 1 to number of threads */
-    /* TODO: 4. Create consumer thread(s) */
-    /* TODO: 5. Main thread sleep */
-    /* TODO: 6. Exit */
-}
+ #include <iostream>
+ #include "buffer.h"
+ #include <unistd.h>
+ #include <pthread.h>
+ #include <cstdlib>
+ #include <ctime>
+ 
+ using namespace std;
+ 
+ // global buffer object
+ Buffer buffer;
+ 
+ // Producer thread function
+ void *producer(void *param) {
+     // Each producer insert its own ID into the buffer
+     buffer_item item = *((int *) param);
+ 
+     while (true) {
+         /* sleep for a random period of time */
+         usleep(rand() % 1000000);
+         
+         if (buffer.insert_item(item)) {
+             cout << "Producer " << item << ": Inserted item " << item << endl;
+             buffer.print_buffer();
+         } else {
+             cout << "Producer error condition" << endl;
+         }
+     }
+ }
+ 
+ // Consumer thread function
+ void *consumer(void *param) {
+     buffer_item item;
+ 
+     while (true) {
+         /* sleep for a random period of time */
+         usleep(rand() % 1000000);
+         
+         if (buffer.remove_item(&item)) {
+             cout << "Consumer Removed item " << item << endl;
+             buffer.print_buffer();
+         } else {
+             cout << "Consumer error condition" << endl;
+         }
+     }
+ }
+ 
+ int main(int argc, char *argv[]) {
+     // Initialize random seed
+     srand(time(NULL));
+     
+     // 1. Get command line arguments
+     if (argc != 4) {
+         cerr << "Usage: " << argv[0] << " <sleep_time> <num_producers> <num_consumers>" << endl;
+         return 1;
+     }
+     
+     int sleep_time = atoi(argv[1]);
+     int num_producers = atoi(argv[2]);
+     int num_consumers = atoi(argv[3]);
+     
+     cout << "Running with parameters:" << endl;
+     cout << "  Sleep time: " << sleep_time << " seconds" << endl;
+     cout << "  Number of producers: " << num_producers << endl;
+     cout << "  Number of consumers: " << num_consumers << endl;
+     
+     // 2. Initialize buffer (buffer is already globally declared)
+     
+     // 3. Create producer threads
+     pthread_t producers[num_producers];
+     int producer_ids[num_producers];
+     
+     for (int i = 0; i < num_producers; i++) {
+         producer_ids[i] = i + 1;  // Producer IDs start from 1
+         if (pthread_create(&producers[i], NULL, producer, &producer_ids[i]) != 0) {
+             cerr << "Error creating producer thread " << i + 1 << endl;
+             return 1;
+         }
+     }
+     
+     // 4. Create consumer threads
+     pthread_t consumers[num_consumers];
+     
+     for (int i = 0; i < num_consumers; i++) {
+         if (pthread_create(&consumers[i], NULL, consumer, NULL) != 0) {
+             cerr << "Error creating consumer thread " << i + 1 << endl;
+             return 1;
+         }
+     }
+     
+     // 5. Main thread sleep
+     cout << "Main thread sleeping for " << sleep_time << " seconds..." << endl;
+     sleep(sleep_time);
+     
+     // 6. Exit (This is a simple exit and doesn't properly clean up threads)
+     cout << "Main thread exiting." << endl;
+     return 0;
+ }
+ 
